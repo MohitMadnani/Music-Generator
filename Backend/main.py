bdd4cb80df9800b7ebb08ebd87d2a68b9e9abf27
Mohit <mohitmadnani419@gmail.com>
2026-01-24 23:52:59 +0000

    Initial commit: Music generator with ACE-Step backend

---
+++ Backend/main.py
@@ -0,0 +1,68 @@
+import uuid
+import modal
+import os 
+
+app = modal.App("music-generator")
+
+image = (
+    modal.Image.debian_slim()
+    .apt_install("git")
+    .pip_install_from_requirements("requirements.txt")
+    .run_commands(["git clone https://github.com/ace-step/ACE-Step.git /tmp/ACE-Step", "cd /tmp/ACE-Step && pip install ."])
+    .env({"HF_HOME": "/.cache/huggingface"})
+    .add_local_python_source("prompts")
+)
+
+model_volume = modal.Volume.from_name("ace-step-models", create_if_missing=True)
+hf_volume = modal.Volume.from_name("qwen-hf-cache", create_if_missing=True)
+
+music_gen_secrets = modal.Secret.from_name("music-gen-secrets")
+
+@app.cls(
+    image = image,
+    gpu = "L40S",
+    volumes = {"/models": model_volume, "/.cache/huggingface": hf_volume},
+    secrets = [music_gen_secrets],
+    scaledown_window = 15
+)
+class MusicGenServer:
+    @modal.enter()
+    def load_model(self):
+        from acestep.pipeline_ace_step import ACEStepPipeline
+        from transformers import AutoTokenizer, AutoModelForCausalLM
+        from diffusers import AutoPipelineForText2Image
+        import torch
+        # Music generator model
+        self.music_model = ACEStepPipeline(
+            checkpoint_dir = "/models",
+            dtype="bfloat16",
+            torch_compile=False,
+            cpu_offload=False,
+            overlapped_decode=False
+        )
+
+        # Large language model
+        model_id = "Qwen/Qwen2-7B-Instruct"
+        self.tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-7B-Instruct")
+
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            torch_dtype="auto",
+            device_map="auto",
+            cache_dir = "/.cache/huggingface"
+        )
+
+        # Stable diffusion model (thumbnails)
+        self.image_pipe = AutoPipelineForText2Image.from_pretrained(
+            "stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16", cache_dir = "/.cache/huggingface")
+        self.image_pipe.to("cuda")
+
+    @modal.fastapi_endpoint(method="POST")
+    def generate(self):
+        output_dir = "/tmp/outputs"
+        os.mkdir(output_dir, exist_ok=True)
+        output_path = os.path.join(output_dir, f"{uuid.uuid4()}.wav")
+@app.local_entrypoint()
+def main():
+    function_test.remote()
+
